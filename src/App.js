daa6064fb996ea5d9d4fb90e594939e774679bec
Alejandro Garcia <agarc371@gmail.com>
2020-10-31 15:36:29 -0400

    update section 1 styling

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-<<<<<<< HEAD
 import Navbar from "./Navbar";
+import Minimenu from "./Minimenu";
 import About from "./components/About";
-=======
-import Navbar from "./Navbar"
-import Minimenu from "./Minimenu"
-
-
->>>>>>> b026ca7947ad2bfbecb46ead07a299f787b38ae4
 
 export default class App extends Component {
   render() {
     return (
-<<<<<<< HEAD
       <div className="ui container">
+        <Minimenu />
         <Navbar />
         <About />
-=======
-      <div className= "ui container">
-        <Minimenu />
-         <Navbar />
-        
->>>>>>> b026ca7947ad2bfbecb46ead07a299f787b38ae4
       </div>
     );
   }
--- src/components/About.js
+++ src/components/About.js
@@ -2,22 +2,34 @@ import React from "react";
 
 export default function About() {
   return (
-    <div class="ui segment">
+    <div class="ui inverted segment">
       <div class="ui two column very relaxed grid">
         <div class="column about-text">
+          <h1>Lorem Ipsum</h1>
           <p>
-            <strong>"</strong>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-            tristique sed lorem a volutpat. Fusce at iaculis nunc. Interdum et
-            malesuada fames ac ante ipsum primis in faucibus. Pellentesque
-            lacus.<strong>"</strong>
+            tristique sed lorem a volutpat.
           </p>
+          <div class="ui hidden section divider image-container">
+            <img class="icon" src="/images/circle-icon.png" alt="icon 1" />
+            <img class="icon" src="/images/circle-icon.png" alt="icon 2" />
+            <img class="icon" src="/images/circle-icon.png" alt="icon 3" />
+            <img class="icon" src="/images/circle-icon.png" alt="icon 4" />
+          </div>
+          <br />
+          <p>
+            Fusce at iaculis nunc. Interdum et malesuada fames ac ante ipsum
+            primis in faucibus. Pellentesque lacus.
+          </p>
+          <br />
+          <br />
+          <button class="massive ui button centerContent">SHOP NOW</button>
         </div>
         <div class="column">
           <img src="sample image" alt="sample about" />
         </div>
       </div>
-      <div class="ui vertical divider"></div>
+      <div class="ui hidden vertical divider"></div>
     </div>
   );
 }
